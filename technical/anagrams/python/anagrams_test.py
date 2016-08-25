bad785aa249f3a3742c857d1c7d5aedb53bac46c
sartechb <sarthak1657@gmail.com>
2016-08-25 13:53:53 -0400

    added python solution for anagram

---
+++ technical/anagrams/python/anagrams_test.py
@@ -0,0 +1,27 @@
+from anagrams import anagram
+
+emptyA = ''
+emptyB = ''
+uneqA = 'length'
+uneqB = 'lengths'
+reflexiveA = 'property'
+reflexiveB = 'property'
+googleA = 'cinema'
+googleB = 'iceman'
+extraA = 'cinema'
+extraB = 'icemam'
+differentA = 'cinema'
+differentB = 'icemat'
+
+
+print anagram(emptyA, emptyB)
+print anagram(uneqA, uneqB)
+print anagram(reflexiveA, reflexiveB)
+print anagram(googleA, googleB)
+print anagram(extraA, extraB)
+print anagram(differentA, differentB)
+
+
+
+
+
